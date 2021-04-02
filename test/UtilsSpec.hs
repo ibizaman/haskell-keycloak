cdd3c6a035fbab68d1b6b44aac840236db6ec464
ibzaman <ibizapeanut@gmail.com>
2021-04-02 08:15:17 -0700

    fix test

--- test/UtilsSpec.hs
+++ test/UtilsSpec.hs
@@ -1,12 +1,17 @@
 module UtilsSpec
-  ( spec,
-  )
-where
+  ( spec
+  ) where
 
-import qualified Test.Hspec as T
-import qualified Utils as U
+import qualified Test.Hspec                    as T
+import qualified Utils                         as U
 
 spec :: T.Spec
 spec = do
   T.describe "splitOn" $ do
-    T.it "returns nothing on empty list" $ U.joinAfter 2 [] `T.shouldBe` Nothing
+    T.it "returns nothing on empty list" $ U.joinTwo [] `T.shouldBe` Nothing
+    T.it "returns nothing on list of one" $ U.joinTwo ["a"] `T.shouldBe` Nothing
+    T.it "returns list of two" $ U.joinTwo ["a", "b"] `T.shouldBe` Just
+      ("a", "b", "")
+    T.it "returns rest on list of more than two"
+      $            U.joinTwo ["a", "b", "c", "d"]
+      `T.shouldBe` Just ("a", "b", "cd")
